b31c82bce3a7ad52f40998e4d797a19174eba02b
ivi0540 <ivi0540@gmail.com>
2024-01-24 23:26:52 +0300

    fix: 8 point of the task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -7,82 +7,97 @@ fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (error) => {
         console.log(error);
     } else {
         // console.log("новая папка создана");
-        fs.mkdir(path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
-            if (error) {
-                console.log(error);
-            };
-        });
     };
+    fs.mkdir(path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
+        if (error) {
+            console.warn(error.message);
+        };
+    });
 });
 
 let articles = "";
 let footer = "";
 let header = "";
+let about = "";
 let template = "";
 fs.readdir(path.join(__dirname, "components"), (error, files) => {
-    files.forEach((file) => {
 
-        fs.readFile(path.join(__dirname, "components", file), "utf-8", (error, textInDoc) => {
-            if (file === "articles.html") {
-                articles = textInDoc;
-            };
-            if (file === "footer.html") {
-                footer = textInDoc;
-            };
-            if (file === "header.html") {
-                header = textInDoc;
-            };
-        });
+    let templateStream = fs.createReadStream(path.join(__dirname, "template.html"));
+    templateStream.on("data", (data) => {
+        template = data.toString();
 
-        fs.readFile(path.join(__dirname, "template.html"), "utf-8", (error, textInTemplate) => {
-            template = textInTemplate;
-            template = template.replace("{{header}}", header);
-            template = template.replace("{{footer}}", footer);
-            template = template.replace("{{articles}}", articles);
-
-            // 2-Замена шабловов и сохдание нового index.js
-            fs.writeFile(path.join(__dirname, "project-dist", "index.html"), template, { recursive: true }, (error) => {
-                if (error) {
-                    console.log(error.message);
-                };
+        if (files.indexOf("articles.html") > -1) {
+            let articlesStream = fs.createReadStream(path.join(__dirname, "components", "articles.html"));
+            articlesStream.on("data", (data) => {
+                articles = data.toString();
+                template = template.replace("{{articles}}", articles);
             });
+        };
+
+        if (files.indexOf("footer.html") > -1) {
+            let footerStream = fs.createReadStream(path.join(__dirname, "components", "footer.html"));
+            footerStream.on("data", (data) => {
+                footer = data.toString();
+                template = template.replace("{{footer}}", footer);
+            });
+        };
+
+        if (files.indexOf("about.html") > -1) {
+            let aboutStream = fs.createReadStream(path.join(__dirname, "components", "about.html"));
+            aboutStream.on("data", (data) => {
+                about = data.toString();
+                template = template.replace("{{about}}", about);
+
+            });
+        };
+        if (files.indexOf("header.html") > -1) {
+            let headerStream = fs.createReadStream(path.join(__dirname, "components", "header.html"));
+            headerStream.on("data", (data) => {
+                header = data.toString();
+                template = template.replace("{{header}}", header);
+                
+                let writeStreamToIndex = fs.createWriteStream(path.join(__dirname, "project-dist", "index.html"));
+                writeStreamToIndex.write(template, (error) => {
+                    if (error) { console.warn(error.message); }
+                });
+            });
+        };
+
+
+    });
+});
+
+// 3-Копирование стилей из шаблона в 1 фаил
+fs.readdir(path.join(__dirname, "styles"), (error, styles) => {
+    fs.writeFile(path.join(__dirname, "project-dist", "style.css"), "", { recursive: true }, (error) => {
+        if (error) {
+            console.warn(error.message);
+        };
+    });
+    styles.forEach((style) => {
+        fs.readFile(path.join(__dirname, "styles", style), "utf-8", (error, textInStyle) => {
+            if (path.extname(path.join(__dirname, "styles", style)) === ".css") {
+                fs.appendFile(path.join(__dirname, "project-dist", "style.css"), textInStyle, { recursive: true }, (error) => {
+                    if (error) {
+                        console.log(error.message);
+                    };
+                });
+            };
         });
     });
+});
 
-    // 3-Копирование стилей из шаблона в 1 фаил
-    fs.readdir(path.join(__dirname, "styles"), (error, styles) => {
-        fs.writeFile(path.join(__dirname, "project-dist", "style.css"), "", { recursive: true }, (error) => {
-            if (error) {
-                console.log(error.message);
-            };
-        });
-        styles.forEach((style) => {
-            fs.readFile(path.join(__dirname, "styles", style), "utf-8", (error, textInStyle) => {
-                if (path.extname(path.join(__dirname, "styles", style)) === ".css") {
-                    fs.appendFile(path.join(__dirname, "project-dist", "style.css"), textInStyle, { recursive: true }, (error) => {
-                        if (error) {
-                            console.log(error.message);
-                        };
-                    });
-                };
-            });
-        });
-    });
-
-    fs.rm(path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
+fs.rm(path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
+    if (error) {
+        // console.log(error.message);
+    } else {
+        // console.log("Удаление прошло успешно");
+    };
+    fs.cp(path.join(__dirname, "assets"), path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
         if (error) {
             console.log(error.message);
         } else {
-            // console.log("Удаление прошло успешно");
+            // console.log("копирование прошло успешно");
         };
-        fs.cp(path.join(__dirname, "assets"), path.join(__dirname, "project-dist", "assets"), { recursive: true }, (error) => {
-            if (error) {
-                console.log(error.message);
-            } else {
-                // console.log("копирование прошло успешно");
-            };
-        });
     });
-
 });
-
